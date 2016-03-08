cd48266dea91a45cf37244c80703ccb3003b713f
Brian Thurlow <bthurlow0424@gmail.com>
2016-03-08 09:30:45 -0600

    Update index.android.js
    Support multiple recipients.

--- index.android.js
+++ index.android.js
@@ -23,18 +23,25 @@ function dial(telNum,prompt) {
 }
 
 function sms(smsNum, messageText) {
-	try {
-		var intent = new android.content.Intent(android.content.Intent.ACTION_VIEW);
-		intent.setData(android.net.Uri.parse("sms:" + smsNum));
-		intent.putExtra("sms_body", messageText);
-		application.android.foregroundActivity.startActivity(intent);
-		return true;
+    return new Promise(function (resolve, reject){
+        if(!smsNum instanceof Array){
+            reject("Numbers are not in an array!");
+        }
+        
+    	try {
+    		var intent = new android.content.Intent(android.content.Intent.ACTION_VIEW);
+            intent.putExtra("address", smsNum.join(";"));
+    		intent.putExtra("sms_body", messageText);
+            intent.setType("vnd.android-dir/mms-sms");
+    		application.android.foregroundActivity.startActivity(intent);
+            resolve({
+                response:"success"
+            });
 
-	} catch(ex) {
-		//alert("Unable to open sms");
-		//console.log("phone.sms failed: " + ex);
-		return false;
-	}
+    	} catch(ex) {
+            reject(ex);
+    	}
+    });
 }
 
 exports.dial = dial;

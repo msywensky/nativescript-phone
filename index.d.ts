348f3afb09c7cc39486184f2ce621901cf9e4d27
bradmartin <bradwaynemartin@gmail.com>
2016-10-24 16:26:32 -0500

    bump

--- index.d.ts
+++ index.d.ts
@@ -3,13 +3,14 @@
  * @param {string} number - The number to dial.
  * @param {boolean} confirm - To enable OS specific confirmation before dialing.
  */
-export function dial(number: string, confirm: boolean);
+export function dial(number: string, confirm: boolean): void;
 
 
 /**
  * Open the OS specific SMS app.
  * @param {Array<string>} numbers - The number to send SMS to.
  * @param {string} message - The message to send.
+ * @returns {Promise} response
  */
 export function sms(numbers: Array<string>, message: string): Promise<response>;
 
